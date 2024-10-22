93d0afd722ee92a10643d4307252ae689ea40aca
Ziad <ziadtariq999@gmail.com>
2024-10-22 16:32:16 -0400

    Add files via upload

---
+++ lab4a.py
@@ -0,0 +1,51 @@
+from machine import Pin
+import time
+
+# GPIO pin setup
+SHCP = Pin(18, Pin.OUT)  # Shift Register Clock
+STCP = Pin(19, Pin.OUT)  # Latch
+DS = Pin(20, Pin.OUT)    # Serial Data Input
+OE = Pin(21, Pin.OUT)    # Output Enable
+
+pattern = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
+
+manual_pattern = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+for pattern1 in manual_pattern:
+    print(pattern1)
+
+
+    #for i in range(0,16):
+        #manual_pattern[i]=1
+        #if (i > 0):
+           # manual_pattern[i-1]=0
+        #print(manual_pattern)
+
+    for j in pattern1:
+        DS.value(j)
+        SHCP.on()
+        SHCP.off()
+
+    DS.value(0)
+
+    STCP.on()
+    STCP.off()
+
+    OE.value(0)
+    time.sleep(0.2)
